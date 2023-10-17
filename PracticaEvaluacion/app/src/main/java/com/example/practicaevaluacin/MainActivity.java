189ec79cf03cacfcf745c25c873cfcde4e74693c
Mabadcortes <mabacor032@g.educaand.es>
2023-10-17 14:15:48 +0200

    Práctica Evaluación

---
+++ PracticaEvaluacion/app/src/main/java/com/example/practicaevaluacin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.practicaevaluacin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pagina_principal);
+    }
+}
